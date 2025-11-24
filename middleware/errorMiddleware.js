74f5c46920612ba22646436e240e8785e6e19dec
rahul rawat <rahulrawathp2004@gmail.com>
2025-11-24 10:42:23 +0530

    errorHandler middleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,7 +2,7 @@
 export const notFound = (req, res, next) => {
   res.status(404).json({ message: `Route ${req.originalUrl} not found` });
 };
-
+// middleware for handling error
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
