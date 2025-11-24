eca9c86655c97db72d2c4d27e599757fe4c71c0c
rahul rawat <rahulrawathp2004@gmail.com>
2025-11-24 10:50:17 +0530

    collaborations router

--- routes/collaborationRoutes.js
+++ routes/collaborationRoutes.js
@@ -10,7 +10,7 @@ import protect from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
-
+// router for collaborations
 router.post('/', protect, createCollaboration);
 router.get('/', protect, getCollaborations);
 router.get('/:id', protect, getCollaborationById);
